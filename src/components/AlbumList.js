78bf10032dfce2bc5b18a27bd77bf602d8df691c
bfazeli <bfazeli1@gmail.com>
2018-10-23 13:12:59 -0700

    Update: Created a card and sectioned off the card into components.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
+import AlbumDetail from './AlbumDetail'
 
 class AlbumList  extends Component {
     state = { albums: [] }
@@ -15,12 +16,18 @@ class AlbumList  extends Component {
             })
     }
 
+    renderAlbums() {
+        return this.state.albums.map(album => 
+            <AlbumDetail key={album.title} album={album} />
+        )
+    }
+
     render() {
         console.log(this.state);
         
         return (
             <View>
-                <Text>Album List!!!</Text>
+                {this.renderAlbums()}
             </View>
         )
     }
